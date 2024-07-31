7c5d3ab786b9d08b7fb639168e9225654e436077
klliio <111129234+klliio@users.noreply.github.com>
2024-07-31 20:16:26 +0100

    Add syncthing

--- nix/system.nix
+++ nix/system.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, userInfo, ... }: {
     # Set your time zone.
     time.timeZone = "Europe/London";
 
@@ -59,6 +59,30 @@
         upower.enable = true;
         power-profiles-daemon.enable = true;
         accounts-daemon.enable = true;
+
+        syncthing = {
+            enable = true;
+            user = userInfo.username;
+            dataDir = "/home/${userInfo.username}/Syncthing";
+            configDir = "/home/${userInfo.username}/.config/syncthing";
+            overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+            overrideFolders = false;     # overrides any folders added or deleted through the WebUI
+            settings = {
+                devices = {
+                    "pixel" = { id = "TA6YGIP-ZPMMDCL-TZ35VAD-VBGDY4E-YJ4QWCL-RDUUA4V-QH7BVRF-73DYGQQ"; };
+                };
+                folders = {
+                    "Passwords" = {         # Name of folder in Syncthing, also the folder ID
+                        path = "/home/${userInfo.username}/Passwords";    # Which folder to add to Syncthing
+                        devices = [ "pixel" ];      # Which devices to share the folder with
+                    };
+                    "Obsidian" = {         # Name of folder in Syncthing, also the folder ID
+                        path = "/home/${userInfo.username}/Obsidian";    # Which folder to add to Syncthing
+                        devices = [ "pixel" ];      # Which devices to share the folder with
+                    };
+                };
+            };
+        };
     };
 
     nix.settings = {
