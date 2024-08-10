cbd6c0f60f8df240a6c96b585201c1e018aa27ba
klliio <111129234+klliio@users.noreply.github.com>
2024-08-10 20:15:41 +0100

    Change disabled to disconnected

--- ags/widgets/network.js
+++ ags/widgets/network.js
@@ -114,7 +114,7 @@ export const NetworkWifiToggle = () =>
     ArrowToggleButton({
         name: 'network',
         icon: wifi.bind('icon_name'),
-        label: wifi.bind('ssid').as((ssid) => ssid || 'Disabled'),
+        label: wifi.bind('ssid').as((ssid) => ssid || 'Disconnected'),
         connection: [wifi, () => wifi.enabled],
         deactivate: () => (wifi.enabled = false),
         activate: () => {
