a3e4ab0b7ffd32924fc55afbedcb4a45b8d33422
klliio <111129234+klliio@users.noreply.github.com>
2024-08-13 17:44:49 +0100

    add automatic store optimisation and gc

--- nix/system.nix
+++ nix/system.nix
@@ -63,6 +63,14 @@
         accounts-daemon.enable = true;
     };
 
+    # automatic store
+    nix.optimise.automatic = true;
+    nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+    };
+
     nix.settings = {
         substituters = ["https://hyprland.cachix.org"];
         trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
