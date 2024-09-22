deda2a1b81c4e888b5955ec82a300ac59cf05f7a
klliio <111129234+klliio@users.noreply.github.com>
2024-09-22 18:25:50 +0100

    fix audio buzzing when idle

--- nix/audio.nix
+++ nix/audio.nix
@@ -4,5 +4,11 @@
     enable = true;
     wireplumber.enable = true;
     pulse.enable = true;
+    wireplumber.extraConfig = {
+      "alsa_output.pci-0000_00_1f.3.analog-stereo" = {
+        pause-on-idle = false;
+        session.suspend-timeout-seconds = 0;
+      };
+    };
   };
 }
