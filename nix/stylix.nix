aeaf5d0aa64bca719ae50246a67271973a8d22fe
klliio <111129234+klliio@users.noreply.github.com>
2024-10-18 21:52:27 +0100

    increase opacity

--- nix/stylix.nix
+++ nix/stylix.nix
@@ -44,7 +44,7 @@
 
             opacity = {
                 applications = 1.0;
-                terminal = 0.9;
+                terminal = 0.95;
                 desktop = 1.0;
                 popups = 1.0;
             };
