55e37c23a3af7a44afa2c0f323cc0d1bd1554efa
klliio <111129234+klliio@users.noreply.github.com>
2024-12-16 03:09:08 +0000

    remove hdmi audio & add alsa persistence

--- nix/audio.nix
+++ nix/audio.nix
@@ -1,13 +1,27 @@
 {
+  hardware.alsa.enablePersistence = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    wireplumber.enable = true;
+    alsa.enable = true;
     pulse.enable = true;
+    wireplumber.enable = true;
     wireplumber.extraConfig = {
-      "alsa_output.pci-0000_00_1f.3.analog-stereo" = {
-        pause-on-idle = false;
-        session.suspend-timeout-seconds = 0;
+      "disable-devices" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {
+                "device.name" = "alsa_card.pci-0000_c4_00.1";
+              }
+            ];
+            actions = {
+              update-props = {
+                "device.disabled" = true;
+              };
+            };
+          }
+        ];
       };
     };
   };
