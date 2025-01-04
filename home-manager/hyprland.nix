9cf058afabd29aa5145e3ce5de11fac08c7f4b62
klliio <111129234+klliio@users.noreply.github.com>
2025-01-04 15:27:18 +0000

    enable hardware cursors

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -45,8 +45,8 @@
                 ];
 
                 env = [
-                    "WLR_NO_HARDWARE_CURSORS,1"
-                    "WLR_RENDERER_ALLOW_SOFTWARE,1"
+                    # "WLR_NO_HARDWARE_CURSORS,1"
+                    # "WLR_RENDERER_ALLOW_SOFTWARE,1"
                     "NIXOS_OZONE_WL,1"
                     "MOZ_ENABLE_WAYLAND,1"
                 ];
