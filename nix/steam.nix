ac2987737faf4d587203354b4ded8ca2b06fe98d
klliio <111129234+klliio@users.noreply.github.com>
2025-04-03 14:45:51 +0100

    add proton-ge

--- nix/steam.nix
+++ nix/steam.nix
@@ -3,5 +3,8 @@
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        extraCompatPackages = [
+            pkgs.proton-ge-bin
+        ];
     };
 }
