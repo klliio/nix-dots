ea43e873407b1d61574f59b6d70b99df610fd8f0
klliio <111129234+klliio@users.noreply.github.com>
2026-03-09 08:38:31 +0000

    change to hdr

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -52,7 +52,7 @@
 
                 monitor = [
                     "eDP-1,1920x1080,auto,1"
-                    "HDMI-A-1,1920x1080,auto,1"
+                    "HDMI-A-1,1920x1080,auto,1, bitdepth, 10, cm, hdr, sdrbrightness, 1, sdrsaturation, 1.07"
                     "HDMI-A-2,1920x1080,auto,1,mirror, HDMI-A-1"
                     "HDMI-A-3,1920x1080,auto,1,mirror, HDMI-A-1"
                 ];
