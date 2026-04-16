8a154a900c9b20aca20f2bf8eed1327b6f2a96fd
klliio <111129234+klliio@users.noreply.github.com>
2026-04-16 18:21:34 +0100

    change lockscreen

--- home-manager/hyprlock.nix
+++ home-manager/hyprlock.nix
@@ -10,46 +10,112 @@
 
             background = [
                 {
-                    path = "screenshot";
-                    blur_passes = 3;
-                    blur_size = 3;
+                    color = "rgba(30, 30, 46, 1.0)";
                 }
             ];
 
-            input-field = [
+            shape = [
+                # surrounding box
                 {
-                    size = "200, 50";
-                    position = "0, -60";
-                    halign = "center";
+                    size = "350, 100";
+                    rounding = 0;
+                    color = "rgba(0, 0, 0, 0.0)";
+                    border_color = "rgba(88, 91, 112, 1.0)";
+                    border_size = 2;
                     valign = "center";
-                    monitor = "";
-                    dots_center = true;
-                    fade_on_empty = false;
-                    font_size = 16;
-                    font_color = "rgb(205, 214, 244)";
-                    inner_color = "rgb(30, 30, 46)";
-                    outer_color = "rgb(116, 199, 236)";
-                    fail_color = "rgb(243, 139, 168)";
-                    check_color = "rgb(166, 227, 161)";
-                    outline_thickness = 3;
-                    placeholder_text = "<tt><b>Password</b></tt>";
+                    halign = "center";
+                    zindex = 1;
+                }
+
+                # remove border sides
+                {
+                    size = "370, 50";
+                    rounding = 0;
+                    color = "rgba(30, 30, 46, 1.0)";
+                    border_color = "rgba(0, 0, 0, 0.0)";
+                    border_size = 0;
+                    valign = "center";
+                    halign = "center";
+                    zindex = 2;
+                }
+
+                # time cutout
+                {
+                    size = "50, 20";
+                    rounding = 0;
+                    color = "rgba(30, 30, 46, 1.0)";
+                    border_color = "rgba(0, 0, 0, 0.0)";
+                    border_size = 0;
+                    valign = "center";
+                    halign = "center";
+                    position = "0, 50";
+                    zindex = 2;
                 }
             ];
 
             label = [
-                #time
+                # time
                 {
-                    position = "0, 0";
-                    halign = "center";
-                    valign = "center";
-                    monitor = "";
-                    color = "rgb(205, 214, 244)";
-                    font_size = 40;
+                    color = "rgba(88, 91, 112, 1.0)";
                     text = "<b><tt>$TIME</tt></b>";
+                    text_align = "center";
+                    valign = "center";
+                    halign = "center";
+                    position = "0, 50";
+                    font_size = 10;
+                    font_family = "monospace";
+                    zindex = 10;
+                }
+
+                # user
+                {
+                    color = "rgba(147, 153, 178, 1.0)";
+                    text = "•_• <b>user:</b> $USER";
+                    valign = "center";
+                    halign = "left";
+                    position = "43%, 1.3%";
+                    font_size = 12;
+                    font_family = "monospace";
+                    zindex = 10;
+                }
+
+                # password
+                {
+                    color = "rgba(147, 153, 178, 1.0)";
+                    text = "<b>password:</b>";
+                    valign = "center";
+                    halign = "left";
+                    position = "43%, -1.3%";
+                    font_size = 12;
+                    font_family = "monospace";
+                    zindex = 10;
                 }
             ];
 
-            animations.enabled = true;
+            input-field = [
+                # time
+                {
+                    outer_color = "rgba(0, 0, 0, 0.0)";
+                    inner_color = "rgba(0, 0, 0, 0.0)";
+                    fail_color = "rgba(0, 0, 0, 0.0)";
+                    check_color = "rgba(0, 0, 0, 0.0)";
+                    valign = "center";
+                    halign = "left";
+                    position = "47.8%, -1.35%";
+                    size = "10%, 25";
+                    outline_thickness = 0;
+                    rounding = 0;
+                    dot_size = 0.8;
+                    dots_center = false;
+                    font_color = "rgba(147, 153, 178, 1.0)";
+                    font_family = "monospace";
+                    placeholder_text = "";
+                    fail_text = "";
+                    zindex = 10;
+                }
+            ];
+
+            animations.enabled = false;
         };
     };
 }
