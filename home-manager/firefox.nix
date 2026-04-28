d19a64df05eb12de6f251ba8f4928c75a070ab0c
klliio <111129234+klliio@users.noreply.github.com>
2026-04-28 23:16:54 +0100

    fix firefox

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -11,12 +11,14 @@
                 id = 0;
                 name = "${userInfo.username}";
                 isDefault = true;
-                extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-                    ublock-origin
-                    firemonkey
-                    keepassxc-browser
-                    sponsorblock
-                ];
+                extensions = {
+                    packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+                        ublock-origin
+                        firemonkey
+                        keepassxc-browser
+                        sponsorblock
+                    ];
+                };
                 settings.extensions.autoDisableScopes = 0;
                 userChrome = "${builtins.readFile ../firefox/userChrome.css}";
                 # userContent = "${builtins.readFile ../firefox/userContent.css}";
