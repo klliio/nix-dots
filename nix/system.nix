159246d5388094676a0da947f36960670a33c106
klliio <111129234+klliio@users.noreply.github.com>
2026-06-08 02:14:06 +0100

    enable 32bit in graphics

--- nix/system.nix
+++ nix/system.nix
@@ -24,6 +24,7 @@
 
     # opengl
     hardware.graphics.enable = true;
+    hardware.graphics.enable32Bit = true;
     hardware.graphics.extraPackages = with pkgs; [
         intel-media-driver
         amf
